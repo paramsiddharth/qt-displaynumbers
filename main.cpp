55fefbcdf258bc97023d0affb8434158bab7c7cb
Param Siddharth <paramsiddhartha@gmail.com>
2020-08-13 11:21:19 +0530

    Fix the widget size

--- main.cpp
+++ main.cpp
@@ -3,17 +3,24 @@
 #include <QPushButton>
 #include <QWidget>
 
+class MyWidget: public QWidget {
+	public:
+		MyWidget(QWidget* parent = 0);
+};
+
+MyWidget::MyWidget(QWidget* parent) {
+	setFixedSize(200, 120);
+
+	QPushButton* quit = new QPushButton(tr("Quit"), this);
+	quit->setGeometry(62, 40, 75, 30);
+	quit->setFont(QFont("Times", 18, QFont::Bold));
+
+	connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+}
+
 int main(int argc, char* argv[]) {
 	QApplication app(argc, argv);
-
-	QWidget window;
-	window.resize(200, 120);
-
-	QPushButton quit("Quit", &window);
-	quit.setFont(QFont("Times", 180, QFont::Bold));
-	quit.setGeometry(10, 40, 180, 40);
-	QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
-
-	window.show();
+	MyWidget widget, widget2;
+	widget.show();
 	return app.exec();
 }
